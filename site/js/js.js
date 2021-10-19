719831e501329ba95b054ee6952b98ec3b63f581
zineb2003 <zineb.zineb.zouzou@gmail.com>
2021-10-19 22:15:33 +0100

    counter

--- site/js/js.js
+++ site/js/js.js
@@ -116,23 +116,151 @@ date.innerHTML = new Date().getFullYear();
 
 //timer
 
-const startDays = 81;
-const startHours = 14;
-const startMinutes = 60;
-const startSecond = 00;
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
 
-//days
-var days = document.querySelector(".days .number");
-days.innerHTML = startDays;
 
-//hours
-var hours = document.querySelector(".hours .number");
-hours.innerHTML = startHours;
+//select element
 
-//minutes
-var minutes = document.querySelector(".minutes .number");
-minutes.innerHTML = startMinutes;
+//the text in desc to change the time
+//the container of the timer
+var countdown = document.querySelector(".count");
+//select the container that container the day hours min...
+var num = document.querySelectorAll(".number");
 
-//second
-var seconds = document.querySelector(".seconds .number");
-seconds.innerHTML = startSecond;
+//select the time that existe in the computer
+
+let tempsNow = new Date();
+console.log(tempsNow);
+
+//select the year
+let yearNow = tempsNow.getFullYear();
+console.log("year " + yearNow);
+
+//select the months
+let monthNow = tempsNow.getMonth();
+console.log("month " + monthNow);
+
+//select the day
+let dayNow = tempsNow.getDate();
+console.log("day " + dayNow);
+
+
+//date au future
+let futureDate = new Date(yearNow, monthNow, dayNow + 30, 12, 60, 60);
+console.log(futureDate);
+
+//select the future year
+let yearFuture = futureDate.getFullYear();
+console.log("year future " + yearFuture);
+
+//select the future months
+let monthFuture = futureDate.getMonth();
+console.log("month future " + monthFuture);
+
+//select the future day
+let dayFuture = futureDate.getDate();
+console.log("day future " + dayFuture);
+
+//select the hour
+let hourFuture = futureDate.getHours();
+console.log("the future hour " + hourFuture);
+
+//select minutes
+let minuteFuture = futureDate.getMinutes();
+console.log("the future minutes " + minuteFuture);
+
+//select hours
+let secondFuture = futureDate.getSeconds();
+console.log("the futre seconds " + secondFuture);
+
+
+//solve the problem of the month and day number
+
+monthFuture = months[monthFuture];
+console.log("the problem of the mounth is solved : " + monthFuture);
+
+const weekday = weekdays[futureDate.getDay()];
+console.log("the problem of the day is solved :: " + weekday);
+
+
+
+
+//select the time now 
+
+let timeFuture = futureDate.getTime();
+console.log("the future time " + timeFuture);
+
+function counter() {
+    let timeNow = new Date().getTime();
+    console.log("time now :::::" + timeNow);
+
+    const t = timeFuture - timeNow;
+
+    console.log("the time counter t : " + t);
+
+    // 1s = 1000ms
+    // 1m = 60s
+    // 1hr = 60m
+    // 1d = 24hr
+    // values in miliseconds
+    oneDay = 24 * 60 * 60 * 1000;
+    oneHour = 60 * 60 * 1000;
+    oneMinute = 60 * 1000;
+
+    let days = Math.floor(t / oneDay);
+    console.log("days :" + days);
+
+    let hours = Math.floor((t % oneDay) / oneHour);
+    console.log("hours :" + hours);
+
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    console.log("minutes :" + minutes);
+
+    let secondes = Math.floor((t % oneMinute) / 1000);
+    console.log("secondes :" + secondes);
+
+    let value = [days, hours, minutes, secondes];
+
+    function format(item) {
+        if (item < 10) {
+            return (item = `0${item}`);
+        } else {
+            return item;
+        }
+    }
+
+    num.forEach(function(item, index) {
+        item.innerHTML = format(value[index]);
+    });
+    if (t < 0) {
+        clearInterval(countdown);
+        countdown.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
+
+}
+let countdownS = setInterval(counter, 1000);
+counter();
+
+//lessons
